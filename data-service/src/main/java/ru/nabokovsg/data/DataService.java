4b3d61527b78589eca0cff7310ccf4af491367e5
Nabokov79 <nabokovsg@yandex.ru>
2024-01-13 14:46:32 +0300

    add

---
+++ data-service/src/main/java/ru/nabokovsg/data/DataService.java
@@ -0,0 +1,12 @@
+package ru.nabokovsg.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataService.class, args);
+    }
+}
---
+++ templates-service/src/main/java/ru/nabokovsg/templates/TemplatesService.java
@@ -0,0 +1,12 @@
+package ru.nabokovsg.templates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatesService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplatesService.class, args);
+    }
+}
