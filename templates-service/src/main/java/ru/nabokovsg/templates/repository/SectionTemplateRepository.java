9aa1adbb7f653de267661331a405f26c13a309f8
NabokovSG <nabokovsg@yandex.ru>
2024-01-18 16:58:39 +0300

    add

--- templates-service/src/main/java/ru/nabokovsg/templates/repository/SectionTemplateRepository.java
+++ templates-service/src/main/java/ru/nabokovsg/templates/repository/SectionTemplateRepository.java
@@ -2,9 +2,11 @@ package ru.nabokovsg.templates.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.nabokovsg.templates.models.SectionTemplate;
 import ru.nabokovsg.templates.models.SubsectionTemplate;
 
+import java.util.List;
 import java.util.Set;
 
 public interface SectionTemplateRepository extends JpaRepository<SectionTemplate, Long> {
